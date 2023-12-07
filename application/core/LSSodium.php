404ea511ffc851cf74421c8d1299af599513b217
Matthew Favetti <mfavetti@gmail.com>
2023-12-07 01:05:10 -0800

    refactor lssodium

--- application/core/LSSodium.php
+++ application/core/LSSodium.php
@@ -6,8 +6,9 @@
 class LSSodium
 {
     public $bLibraryExists = false;
-    protected $sEncryptionNonce = null;
-    protected $sEncryptionSecretBoxKey = null;
+    protected $key = null;
+    // static nonce, only used for fallback decryption
+    protected $nonce = null;
 
     public function init()
     {
@@ -15,118 +16,64 @@ class LSSodium
         require_once Yii::app()->basePath . '/../vendor/paragonie/sodium_compat/src/Core/Util.php';
         require_once Yii::app()->basePath . '/../vendor/paragonie/sodium_compat/autoload.php';
 
-        $this->checkIfLibraryExists();
+        // set availability of sodium library
+        // this is a public property and referenced elsewhere
+        $this->bLibraryExists = function_exists('sodium_crypto_sign_open') === true;
 
         if ($this->bLibraryExists === false) {
             /*throw new SodiumException(sprintf(gT("This operation uses data encryption functions which require Sodium library to be installed, but library was not found. If you don't want to use data encryption, you have to disable encryption in attribute settings. Here is a link to the manual page:
             %s", 'unescaped'), 'https://manual.limesurvey.org/Data_encryption#Errors'));*/
         } else {
-            $this->checkIfKeyExists();
+            // get existing key from config, default is empty string
+            $key = Yii::app()->getConfig('encryptionsecretboxkey');
+            // if the key is empty, generate a new one
+            if (empty($key)) {
+                $this->generateEncryptionKeys();
+            }
+            // use existing key
+            else {
+                $this->key = ParagonIE_Sodium_Compat::hex2bin((string) $key);
+
+                // load static nonce if exists in config in order to use for fallback decryption, otherwise null
+                $nonce = Yii::app()->getConfig('encryptionnonce');
+                $this->nonce = empty($nonce) ? null : ParagonIE_Sodium_Compat::hex2bin((string) $nonce);
+            }
         }
-    }
-
-    /**
-     * Check if Sodium library is installed
-     * @return void
-     */
-    public function checkIfLibraryExists()
-    {
-        if (function_exists('sodium_crypto_sign_open')) {
-            $this->bLibraryExists = true;
-        }
-    }
-
-    /**
-     *
-     * Check if encryption key and nonce exist in configuration and generate it if missing
-     * @return void
-     * @throws SodiumException
-     */
-    protected function checkIfKeyExists()
-    {
-        if (empty(App()->getConfig('encryptionsecretboxkey')) && empty(App()->getConfig('encryptionnonce'))) {
-            $this->generateEncryptionKeys();
-        }
-        if ($this->sEncryptionNonce === null) {
-            $this->sEncryptionNonce = $this->getEncryptionNonce();
-        }
-        if ($this->sEncryptionSecretBoxKey === null) {
-            $this->sEncryptionSecretBoxKey = $this->getEncryptionSecretBoxKey();
-        }
-    }
-
-    /**
-     * @return string
-     * @throws SodiumException
-     */
-    protected function getEncryptionNonce()
-    {
-        return sodium_hex2bin((string) Yii::app()->getConfig('encryptionnonce'));
-    }
-
-    /**
-     * @return string
-     * @throws SodiumException
-     */
-    protected function getEncryptionSecretBoxKey()
-    {
-        return sodium_hex2bin((string) Yii::app()->getConfig('encryptionsecretboxkey'));
-    }
-
-    /**
-     *
-     * Get encryption key from version.php config file
-     * @return string Return encryption key string
-     * @throws SodiumException
-     */
-    protected function getEncryptionKey()
-    {
-        return ParagonIE_Sodium_Compat::hex2bin(Yii::app()->getConfig('encryptionkeypair'));
-    }
-
-    /**
-     *
-     * Get encryption key from version.php config file
-     * @return string Return encryption key string
-     * @throws SodiumException
-     */
-    protected function getEncryptionPublicKey()
-    {
-        return ParagonIE_Sodium_Compat::hex2bin(Yii::app()->getConfig('encryptionpublickey'));
-    }
-
-    /**
-     *
-     * Get encryption key from version.php config file
-     * @return string Return encryption key string
-     * @throws SodiumException
-     */
-    protected function getEncryptionSecretKey()
-    {
-        return ParagonIE_Sodium_Compat::hex2bin(Yii::app()->getConfig('encryptionsecretkey'));
     }
 
     /**
      * Encrypt input data using AES256 CBC encryption
-     * @param string $sDataToEncrypt Data to encrypt. Could be a string or a serializable PHP object
-     * @return string Return encrypted AES256 CBC value
+     * @param string $plaintext Data to encrypt. Could be a string or a serializable PHP object
+     * @return string Return concatenated hex representation of random nonce and ciphertext
      * @throws SodiumException
      */
-    public function encrypt($sDataToEncrypt): string
+    public function encrypt($plaintext): string
     {
         if ($this->bLibraryExists === true) {
-            if (isset($sDataToEncrypt) && $sDataToEncrypt !== "") {
+            if (isset($plaintext) && $plaintext !== "") {
+
+                // return base64_encode(
+                //     ParagonIE_Sodium_Compat::crypto_secretbox(
+                //         (string) $plaintext,
+                //         $this->nonce,
+                //         $this->key
+                //     )
+                // );
+
+                // generate a random nonce
                 $nonce = random_bytes(ParagonIE_Sodium_Compat::CRYPTO_SECRETBOX_NONCEBYTES);
-                $encrypted = ParagonIE_Sodium_Compat::crypto_secretbox(
-                    (string) $sDataToEncrypt,
+                // encrypt plaintext with key and nonce
+                $ciphertext = ParagonIE_Sodium_Compat::crypto_secretbox(
+                    (string) $plaintext,
                     $nonce,
-                    $this->sEncryptionSecretBoxKey
+                    $this->key
                 );
-                return ParagonIE_Sodium_Compat::bin2hex($nonce) . ParagonIE_Sodium_Compat::bin2hex($encrypted);
+                // concatenate random nonce and cipher text as hex
+                return ParagonIE_Sodium_Compat::bin2hex($nonce) . ParagonIE_Sodium_Compat::bin2hex($ciphertext);
             }
             return '';
         }
-        return $sDataToEncrypt;
+        return $plaintext;
     }
 
     /**
@@ -141,21 +88,27 @@ class LSSodium
     {
         if ($this->bLibraryExists === true) {
             if (!empty($sEncryptedString) && $sEncryptedString !== 'null') {
-                // attempt to decrypt string with the old method of using
-                // a static nonce
-                $plaintext = ParagonIE_Sodium_Compat::crypto_secretbox_open(
-                    base64_decode($sEncryptedString),
-                    $this->sEncryptionNonce,
-                    $this->sEncryptionSecretBoxKey
-                );
+                // assume decryption is not possible
+                $plaintext = false;
                 // minimum length (assuming empty message) is size of nonce
                 // plus the size of the authentication tag
                 $minLength = (
                     ParagonIE_Sodium_Compat::CRYPTO_SECRETBOX_NONCEBYTES +
                     ParagonIE_Sodium_Compat::CRYPTO_SECRETBOX_MACBYTES
                 );
+                // attempt to decrypt string with the old method of using
+                // a static nonce and base 64 encoding
+                if ($this->nonce !== null) {
+                    $plaintext = ParagonIE_Sodium_Compat::crypto_secretbox_open(
+                        base64_decode($sEncryptedString),
+                        $this->nonce,
+                        $this->key
+                    );
+                }
                 // fall through to new method
-                // split the string into nonce and cipher text and decrypt
+                // check that encrypted string is of sufficient length to
+                // contain at minimum the random nonce and authentication tag
+                // split the string into nonce and cipher text then decrypt
                 if ($plaintext === false && strlen($sEncryptedString) >= $minLength) {
                     $nonceAndCipherText = ParagonIE_Sodium_Compat::hex2bin($sEncryptedString);
                     $nonce = substr($nonceAndCipherText, 0, ParagonIE_Sodium_Compat::CRYPTO_SECRETBOX_NONCEBYTES);
@@ -163,7 +116,7 @@ class LSSodium
                     $plaintext = ParagonIE_Sodium_Compat::crypto_secretbox_open(
                         $ciphertext,
                         $nonce,
-                        $this->sEncryptionSecretBoxKey
+                        $this->key
                     );
                 }
                 // neither method worked, error
@@ -186,22 +139,14 @@ class LSSodium
      */
     protected function generateEncryptionKeys()
     {
-        // commented out to be able to generate new keys for encryption update, also this function will only be executed if no key is available and is redundant.
-//        if (is_file(APPPATH . 'config/security.php')) {
-//            // Never replace an existing file
-//            throw new CException(500, gT("Configuration file already exist"));
-//        }
-        $sEncryptionNonce = sodium_bin2hex(random_bytes(ParagonIE_Sodium_Compat::CRYPTO_SECRETBOX_NONCEBYTES));
-        $sEncryptionSecretBoxKey = sodium_bin2hex(ParagonIE_Sodium_Compat::crypto_secretbox_keygen());
-        // old keys used for encryption are still available as a backup if they have been used before
-        $sEncryptionKeypair = sodium_bin2hex($this->getEncryptionKey());
-        $sEncryptionPublicKey = sodium_bin2hex($this->getEncryptionPublicKey());
-        $sEncryptionSecretKey = sodium_bin2hex($this->getEncryptionSecretKey());
-
-        if (empty($sEncryptionNonce) || empty($sEncryptionSecretBoxKey)) {
-            return;
-        }
-
+        // generate new key
+        $newKeyBin = ParagonIE_Sodium_Compat::crypto_secretbox_keygen();
+        $newKeyHex = ParagonIE_Sodium_Compat::bin2hex($newKeyBin);
+        // update app config
+        Yii::app()->setConfi("encryptionsecretboxkey", $newKeyHex);
+        // set class property
+        $this->key = $newKeyBin;
+        // generate config for security.php
         $sConfig = "<?php if (!defined('BASEPATH')) exit('No direct script access allowed');" . "\n"
             . "/*" . "\n"
             . " * LimeSurvey" . "\n"
@@ -222,21 +167,10 @@ class LSSodium
             . "*/" . "\n"
             . "\n"
             . "\$config = array();" . "\n";
-        if ($sEncryptionKeypair) {
-            $sConfig .= "\$config['encryptionkeypair'] = '" . $sEncryptionKeypair . "';" . "\n";
-        }
-        if ($sEncryptionPublicKey) {
-            $sConfig .= "\$config['encryptionpublickey'] = '" . $sEncryptionPublicKey . "';" . "\n";
-        }
-        if ($sEncryptionSecretKey) {
-            $sConfig .= "\$config['encryptionsecretkey'] = '" . $sEncryptionSecretKey . "';" . "\n";
-        }
-        $sConfig .= "\$config['encryptionnonce'] = '" . $sEncryptionNonce . "';" . "\n"
-            . "\$config['encryptionsecretboxkey'] = '" . $sEncryptionSecretBoxKey . "';" . "\n"
-            . "return \$config;";
-
-        Yii::app()->setConfig("encryptionnonce", $sEncryptionNonce);
-        Yii::app()->setConfig("encryptionsecretboxkey", $sEncryptionSecretBoxKey);
+        // persist new secret to security.php
+        $sConfig .= "\$config['encryptionsecretboxkey'] = '" . $newKeyHex. "';" . "\n";
+        $sConfig .= "return \$config;\n";
+        // write out security.php file
         $configdir = \Yii::app()->getConfig('configdir');
         if (is_writable($configdir)) {
             file_put_contents($configdir . '/security.php', $sConfig);
